99b6cd0090e1655d6f51a15a9cceedf4b6975531
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-05-31 17:30:05 -0500

    coding updates

--- TempConversion/Form1.Designer.cs
+++ TempConversion/Form1.Designer.cs
@@ -28,27 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_fahrenheit = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.txt_fahrenheit = new System.Windows.Forms.TextBox();
             this.txt_celsuis = new System.Windows.Forms.TextBox();
             this.txt_kelvin = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.btn_celsuis = new System.Windows.Forms.Button();
             this.btn_kelvin = new System.Windows.Forms.Button();
+            this.btn_fahrenheit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btn_fahrenheit
+            // label1
             // 
-            this.btn_fahrenheit.AutoSize = true;
-            this.btn_fahrenheit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_fahrenheit.ForeColor = System.Drawing.Color.Coral;
-            this.btn_fahrenheit.Location = new System.Drawing.Point(25, 39);
-            this.btn_fahrenheit.Name = "btn_fahrenheit";
-            this.btn_fahrenheit.Size = new System.Drawing.Size(101, 24);
-            this.btn_fahrenheit.TabIndex = 0;
-            this.btn_fahrenheit.Text = "Fahrenheit";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Coral;
+            this.label1.Location = new System.Drawing.Point(25, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fahrenheit";
             // 
             // label2
             // 
@@ -79,6 +79,7 @@
             this.txt_fahrenheit.Name = "txt_fahrenheit";
             this.txt_fahrenheit.Size = new System.Drawing.Size(118, 29);
             this.txt_fahrenheit.TabIndex = 3;
+            this.txt_fahrenheit.TextChanged += new System.EventHandler(this.onTextChange);
             // 
             // txt_celsuis
             // 
@@ -97,16 +98,6 @@
             this.txt_kelvin.TabIndex = 5;
             this.txt_kelvin.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(287, 44);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Convert";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // btn_celsuis
             // 
             this.btn_celsuis.Location = new System.Drawing.Point(287, 94);
@@ -115,6 +106,7 @@
             this.btn_celsuis.TabIndex = 7;
             this.btn_celsuis.Text = "Convert";
             this.btn_celsuis.UseVisualStyleBackColor = true;
+            this.btn_celsuis.Click += new System.EventHandler(this.btn_celsuis_Click);
             // 
             // btn_kelvin
             // 
@@ -124,21 +116,33 @@
             this.btn_kelvin.TabIndex = 8;
             this.btn_kelvin.Text = "Convert";
             this.btn_kelvin.UseVisualStyleBackColor = true;
+            this.btn_kelvin.Click += new System.EventHandler(this.btn_kelvin_Click);
+            // 
+            // btn_fahrenheit
+            // 
+            this.btn_fahrenheit.Location = new System.Drawing.Point(287, 39);
+            this.btn_fahrenheit.Name = "btn_fahrenheit";
+            this.btn_fahrenheit.Size = new System.Drawing.Size(75, 23);
+            this.btn_fahrenheit.TabIndex = 9;
+            this.btn_fahrenheit.Text = "Convert";
+            this.btn_fahrenheit.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btn_fahrenheit.UseVisualStyleBackColor = true;
+            this.btn_fahrenheit.Click += new System.EventHandler(this.btn_fahrenheit_Click);
             // 
             // txt_
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(408, 210);
+            this.Controls.Add(this.btn_fahrenheit);
             this.Controls.Add(this.btn_kelvin);
             this.Controls.Add(this.btn_celsuis);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.txt_kelvin);
             this.Controls.Add(this.txt_celsuis);
             this.Controls.Add(this.txt_fahrenheit);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.btn_fahrenheit);
+            this.Controls.Add(this.label1);
             this.Name = "txt_";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -148,15 +152,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Label btn_fahrenheit;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txt_fahrenheit;
         private System.Windows.Forms.TextBox txt_celsuis;
         private System.Windows.Forms.TextBox txt_kelvin;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btn_celsuis;
         private System.Windows.Forms.Button btn_kelvin;
+        private System.Windows.Forms.Button btn_fahrenheit;
     }
 }
 
--- TempConversion/Form1.cs
+++ TempConversion/Form1.cs
@@ -22,9 +22,45 @@ namespace TempConversion
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+
+        private void btn_celsuis_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btn_kelvin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_fahrenheit_Click(object sender, EventArgs e)
+        {
+            convertFromFahrenheit();
+        }
+
+        private void convertFromFahrenheit()
+        {
+            float f = 0;
+
+            try
+            {
+                f = float.Parse(txt_fahrenheit.Text); //convert number to string
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            float c = 5f / 9f * (f - 32);
+            float k = c - 273;
+
+            txt_celsuis.Text = c.ToString(); //string is going to work wit the text field
+            txt_kelvin.Text = k.ToString();
+        }
+
+        private void onTextChange(object sender, EventArgs e)
+        {
+            convertFromFahrenheit();
         }
     }
 }
